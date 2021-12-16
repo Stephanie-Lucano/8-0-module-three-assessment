5b0569cf698698c69e56d733a3beeafc68b8d724
stephanie lucano <stephaniemarielucano@gmail.com>
2021-12-16 11:38:36 -0500

    Added switch and routes to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import "./App.css";
+import { Component } from "react";
+import Navbar from "./Components/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Movies from "./Components/Movies";
+import People from "./Components/People";
+import Locations from "./Components/Locations";
 
-function App() {
-  return (
-    <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/people" component={People} />
+          <Route path="/locations" component={Locations} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
