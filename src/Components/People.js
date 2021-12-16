4d62ae733a87026700dd093e96112a607b98a50d
stephanie lucano <stephaniemarielucano@gmail.com>
2021-12-16 17:02:57 -0500

    Fetch characters

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,10 +1,51 @@
 import { Component } from "react";
 
 class People extends Component {
-   render() {
+  constructor() {
+    super()
+    this.state = {
+      userInput: "",
+      characters: [],
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      userInput: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("https://ghibliapi.herokuapp.com/people")
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                characters: data,
+            })
+        })
+        .catch((error) => {
+            throw error;
+        })
+        event.target.value = '';
+  }
+
+  render() {
+    const findCharacter = () => {this.state.characters.find((character) => { return character.name === this.state.userInput || null})
+    }
     return (
-      <div className="People">
-          <h1>This is the people's page</h1>
+      <div className="people">
+          <h1>Search for a Person</h1>
+          <form>
+            <input 
+              type='text' 
+              placeholder='Find Your Person' 
+              name='searchbar' 
+              onChange={this.handleChange} 
+              required 
+            />
+            <button onSubmit={this.handleSubmit}>Submit</button>
+          </form>
       </div>
     );
   }
