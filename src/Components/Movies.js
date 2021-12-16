edc8c7b45531944e3c0a5267d98fd30067d3be7f
stephanie lucano <stephaniemarielucano@gmail.com>
2021-12-16 16:27:20 -0500

    Populated the dropdown menus with movies

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,10 +1,47 @@
 import { Component } from "react";
+// import Film from "./Film";
 
 class Movies extends Component {
+  constructor() {
+    super();
+    this.state ={
+      films: [],
+      selectedFilm: "",
+    }
+  }
+
+  fetchFilms = () => {
+    fetch("https://ghibliapi.herokuapp.com/films")
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({films: data})
+      console.log({data})
+    })
+    .catch((error) => {
+      throw error;
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({selectedFilm: event.target.value})
+  }
+
+  componentDidMount = () => {
+    this.fetchFilms()
+  }
+  
   render() {
     return (
-      <div className="Movies">
-          <h1>This is the movies page</h1>
+      <div className="movies">
+          <h1>Select a Movie</h1>
+          <select onChange={this.handleChange}>
+            <option value=""></option>
+            {this.state.films.map((film) => {
+              return (
+                <option value={film.title}>{film.title}</option>
+              )
+            })}
+          </select>
       </div>
     );
   }
